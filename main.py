5e22fb97fd46e6a2d6a3c26946372812baeced2f
arbope <160049420+arbope@users.noreply.github.com>
2024-11-26 12:26:50 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,12 +9,26 @@ import time
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)  
 
-driver.get("https://www.songsterr.com/")
+def skipInterruptions():
+    while True:
+        try: 
+            WebDriverWait(driver, 0.01).until(
+                EC.element_to_be_clickable((By.XPATH , "//a[text()='continue with interruptions']"))
+            ).click()
+        except Exception:
+            time.sleep(0.5)
+
+def goToMainDomain():
+    while True:
+        try: 
+            WebDriverWait(driver, 0.01).until(
+                EC.element_to_be_clickable((By.XPATH , "/html/body/div[1]/div[2]/div/a"))
+            ).click()
+        except Exception:
+            time.sleep(1)
 
 while True:
-    try: 
-        WebDriverWait(driver, 0.01).until(
-            EC.element_to_be_clickable((By.XPATH , "//a[text()='continue with interruptions']"))
-        ).click()
-    except Exception:
-        time.sleep(0.1)
+    if driver.current_url[0:26] == "https://www.songsterr.com/":
+        skipInterruptions()
+    if driver.current_url == "https://dominioshdfull.com/":
+        goToMainDomain()
